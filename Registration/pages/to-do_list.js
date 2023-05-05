7b6ef02d7cc174e3dfc85a94649c68f308f383db
miss-sunflower <otasowie9072@student.babcock.edu.ng>
2023-05-05 14:02:45 +0100

    wrote a function to add tasks to an array

---
+++ Registration/pages/to-do_list.js
@@ -0,0 +1,8 @@
+let addtask = [];
+let i = 1;
+ for(i= 20; i <= 1; i--){
+let task = prompt('What are you to achieve?');
+addtask.push(task); //anything the user adds to the list
+console.log(addtask.join('-'));
+ }
+
