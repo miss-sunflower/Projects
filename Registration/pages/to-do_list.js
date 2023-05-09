ebdc31ee21254f14d6eb71c12b34d8e53337a14b
miss-sunflower <otasowie9072@student.babcock.edu.ng>
2023-05-09 15:15:40 +0100

    changed the button name

--- Registration/pages/to-do_list.js
+++ Registration/pages/to-do_list.js
@@ -1,8 +1,13 @@
 let addtask;
-addtask = [];
-for(i= 20; i <= 1; i--){
-let task = prompt('What are you to achieve?');
+addtask = ['help', 'laundry'];
+let task = 'task to be added';      
+//for(i= 20; i <= 1; i--){
+//let task = prompt('What are you to achieve?');
 addtask.push(task); //anything the user adds to the list
 console.log(addtask.join('-'));
-}
+//}
+
+// there has to be some sort of pseudo code
+let removetask = [addtask.pop()]; //hopefully whatever leaves addtask ends up here
+
 
